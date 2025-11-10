41e3515da81b10adfdf9e80a8fbb0f9795f76589
Sergey Yakovlev <noctus@proton.me>
2025-11-10 17:17:26 +0400

    (misc) dependencies update

--- pkg/utils/config/config.go
+++ pkg/utils/config/config.go
@@ -7,7 +7,7 @@ import (
 	"github.com/dalet-oss/opensearch-cli/pkg/utils/flagutils"
 	"github.com/dalet-oss/opensearch-cli/pkg/utils/logging"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	"os"
 )
 
